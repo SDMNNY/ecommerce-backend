0563879992d2af37106f6ca3f2c555dc14e89816
sdmnny <sdmnny@gmail.com>
2022-06-18 04:03:36 -0400

    finalized category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -64,6 +64,20 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-});
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
 
+    if (!categoryData) {
+      res.status(404).json({ message: "This category does not exist."});
+      return;
+    }
+    res.status(200).json(categoryData); 
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
