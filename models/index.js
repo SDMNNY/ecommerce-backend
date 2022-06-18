e64646a4229968cb2cf4353ed50ad38c202e502c
sdmnny <sdmnny@gmail.com>
2022-06-18 01:06:31 -0400

    completed index.js for models

--- models/index.js
+++ models/index.js
@@ -5,10 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKEY: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKEY: "category_id",
+  onDelete: "CASCADE"
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Tag, {
+  through:{
+    model: ProductTag,
+    unique: false
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
 
